e59e5ef17aea3e63b8acf3e26f30f570ed311dda
Jerry W <jerrywiltse@gmail.com>
2017-08-14 21:39:08 -0400

    mass update - minor recipe and cosmetic readme

--- conanfile.py
+++ conanfile.py
@@ -3,8 +3,8 @@ from conans import ConanFile, tools, os
 class BoostComputeConan(ConanFile):
     name = "Boost.Compute"
     version = "1.64.0"
-    generators = "txt"
-    url = "https://github.com/boostorg/compute"
+    url = "https://github.com/bincrafters/conan-boost-compute"
+    source_url = "https://github.com/boostorg/compute"
     description = "Please visit http://www.boost.org/doc/libs/1_64_0/libs/libraries.htm"
     license = "www.boost.org/users/license.html"
     lib_short_name = "compute"
@@ -40,7 +40,7 @@ class BoostComputeConan(ConanFile):
                       
     def source(self):
         self.run("git clone --depth=50 --branch=boost-{0} {1}.git"
-                 .format(self.version, self.url))
+                 .format(self.version, self.source_url))
 
     def package(self):
         include_dir = os.path.join(self.build_folder, self.lib_short_name, "include")
