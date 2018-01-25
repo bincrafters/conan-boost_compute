a7325fe4fc961a9d41c81f06bae7d8b6e4ca5555
Jerry W <jerrywiltse@gmail.com>
2018-01-25 01:12:55 -0500

    Refresh round 2

--- conanfile.py
+++ conanfile.py
@@ -8,7 +8,8 @@ class BoostComputeConan(ConanFile):
     name = "boost_compute"
     version = "1.66.0"
     url = "https://github.com/bincrafters/conan-boost_compute"
-
+    author = "Bincrafters <bincrafters@gmail.com>"
+    exports = ["LICENSE.md"]
     lib_short_names = ["compute"]
     is_header_only = True
 
